aabcaa5108eb4173de3437bae6aadc2f8bd64c8a
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2026-05-22 20:45:07 +0530

    Setup Spring Boot backend and PostgreSQL connection

---
+++ cms-backend/src/test/java/com/cms/cms_backend/CmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.cms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
